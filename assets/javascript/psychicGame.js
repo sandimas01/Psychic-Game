ffb7398c1df9f1ecbed2087bfeea31e185ac30c1
@Donskoy <diman@arocketmail.com>
2018-03-05 05:03:31 -0600

    may 'better var html #game display

--- assets/javascript/psychicGame.js
+++ assets/javascript/psychicGame.js
@@ -1,12 +1,5 @@
-// i need some for loops to get 'guesses left' working
-// and get 'losses counter' working correctly 
-//press letters 9 times
 
-// push in the array user guesses
-
-
-
-    // Creates an array that lists out all of the options (various letters).
+// Creates an array that lists out all of the options (various letters).
     var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     
     // Creating variables to hold the number of wins, losses, and ties. They start at 0.
@@ -17,23 +10,7 @@
     var computerGuess = [];
 
 
-
-
-// Set up an array of numbers.
-//var nums = [1, 8, 2, 3, 1, 1, 6, 6, 6];
-
-    
-    // Creating an empty array for our favorite TV shows.
-    // var favTVshows = [];
-    // var tvShow;
-    
-    // --------------------------------------------------------------------------
-
-
     // This function is run whenever the user presses a key.
-    // Determines which key was pressed.
-    //var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); ***save for later***
-
     document.onkeyup = function(event) {
       var userGuess = event.key;
       lettersGuessed.push(userGuess);
@@ -60,58 +37,15 @@
             losses++;
 
         }
-        //----------------------------------------------------------------------------
-// Set up an array of numbers.
-      //var nums = [1, 8, 2, 3, 1, 1, 6, 6, 6];
-
-    //   // Loop through the array we just set up.
-    //   for (var i = 0; i < computerChoices.length; i++) {
-    //     // If the current number is greater than 2..
-    //     if (computerChoices[i] > 2) {
-    //       // Log "number is greater than 2".
-    //       console.log("number is greater than 2");
-    //     }
-    //     // Otherwise
-    //     else {
-    //       // Log "number is not greater than 2"
-    //       console.log("number is not greater than 2");
-    //     }
-    //   }
-
-//  // For-Loop that will repeat three times.
-//  for (var i = 1; i < 4; i++) {
-
-//    // Each time it asks the user for their #1, #2, or #3 TV show.
-//    tvShow = prompt("What's your #" + i + " favorite TV show?");
-
-//    // It then takes the user's response and "pushes" (or adds) the variable to the end of the favTVshows array.
-//    favTVshows.push(tvShow);
-//  }
-
-
-//  // We then loop through the favTVshows array...
-//  for (var j = 0; j < favTVshows.length; j++) {
-
-//    // ...and create alert messages listing out each show in our array.
-//    console.log(favTVshows[j]);
-//  }
-
-
-
- //--------------------------------------------------------------------------------------------
 
         // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
         var html =
-          "<p>You chose: " + userGuess + "</p>" +
-          "<p>The computer chose: " + computerGuess + "</p>" +
+        "<p>The computer chose: " + computerGuess + "</p>" +
+        "<p>You chose: " + userGuess + "</p>" +
           "<p>wins: " + wins + "</p>" +
-          "<p>Guesses Left: " + guessesLeft + "</p>";
-          "<p>losses: " + losses + "</p>";
+          "<p>losses: " + losses + "</p>" +
+          "<p>Guesses Left: " + guessesLeft + "</p>" +
           "<p>Your guesses so far: " + lettersGuessed + "</p>";
-
-         // "<p>losses: " + nums + "</p>";
-        //   "<p>losses: " + favTVshows + "</p>";
-        //   "<p>losses: " + tvShow + "</p>";
 
         // Set the inner HTML contents of the #game div to our html string
         document.querySelector("#game").innerHTML = html;
