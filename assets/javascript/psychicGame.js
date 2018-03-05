01f6e985a46c9804dcc12d29add499a3f39432f7
@Donskoy <diman@arocketmail.com>
2018-03-05 01:09:02 -0600

    wins and somewhat losses counter working

--- assets/javascript/psychicGame.js
+++ assets/javascript/psychicGame.js
@@ -1,79 +1,43 @@
-// // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-// var computerChoices = ["r", "p", "s"];
 
-// // This function is run whenever the user presses a key.
-// document.onkeyup = function(event) {
-
-//   // Determines which key was pressed.
-//   var userGuess = event.key;
-
-//   // Randomly chooses a choice from the options array. This is the Computer's guess.
-//   var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-//   // Only run the following code block if the user presses "r" or "p" or "s".
-//   if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
-
-//     // Alert the userGuess and computerGuess
-//     alert("User guess: " + userGuess);
-//     alert("Computer guess: " + computerGuess);
-
-//   }
-// };
-//-----------------------------
-
-
-    // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+    // Creates an array that lists out all of the options (various letters).
     var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     // Creating variables to hold the number of wins, losses, and ties. They start at 0.
     var wins = 0;
     var losses = 0;
-    var ties = 0;
+    
+
+    // --------------------------------------------------------------------------
 
     // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
-
       // Determines which key was pressed.
       var userGuess = event.key;
-
-      //var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+      //var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); ***save for later***
         console.log(userGuess)
-
       // Randomly chooses a choice from the options array. This is the Computer's guess.
       var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
         console.log(computerGuess);
 
-        
+
       // Reworked our code from last step to use "else if" instead of lots of if statements.
 
       // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-      if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
-
-        if ((userGuess === "r") && (computerGuess === "s")) {
+        if (userGuess === computerGuess) {
           wins++;
-        } else if ((userGuess === "r") && (computerGuess === "p")) {
-          losses++;
-        } else if ((userGuess === "s") && (computerGuess === "r")) {
-          losses++;
-        } else if ((userGuess === "s") && (computerGuess === "p")) {
-          wins++;
-        } else if ((userGuess === "p") && (computerGuess === "r")) {
-          wins++;
-        } else if ((userGuess === "p") && (computerGuess === "s")) {
-          losses++;
-        } else if (userGuess === computerGuess) {
-          ties++;
         }
+        else if(userGuess !== computerGuess) {
+            losses++;
+        };
 
         // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
         var html =
           "<p>You chose: " + userGuess + "</p>" +
           "<p>The computer chose: " + computerGuess + "</p>" +
           "<p>wins: " + wins + "</p>" +
-          "<p>losses: " + losses + "</p>" +
-          "<p>ties: " + ties + "</p>";
+          "<p>losses: " + losses + "</p>";
 
         // Set the inner HTML contents of the #game div to our html string
         document.querySelector("#game").innerHTML = html;
       }
-    };
+    
