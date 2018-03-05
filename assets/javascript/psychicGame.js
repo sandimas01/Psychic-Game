7f6ac18c3071ac1d657bdb66194f3667e8159a9b
@Donskoy <diman@arocketmail.com>
2018-03-05 03:26:20 -0600

    may 'guesses left display in console

--- assets/javascript/psychicGame.js
+++ assets/javascript/psychicGame.js
@@ -1,6 +1,6 @@
 // i need some for loops to get 'guesses left' working
 // and get 'losses counter' working correctly 
-
+//press letters 9 times
 
 
 
@@ -10,7 +10,15 @@
     // Creating variables to hold the number of wins, losses, and ties. They start at 0.
     var wins = 0;
     var losses = 0;
+    var guessesLeft = 9;
 
+
+
+
+
+
+// Set up an array of numbers.
+//var nums = [1, 8, 2, 3, 1, 1, 6, 6, 6];
 
     
     // Creating an empty array for our favorite TV shows.
@@ -19,8 +27,11 @@
     
     // --------------------------------------------------------------------------
 
+
     // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
+        
+  
       // Determines which key was pressed.
       var userGuess = event.key;
       //var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); ***save for later***
@@ -29,34 +40,35 @@
       var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
         console.log(computerGuess);
 
-
-      // Reworked our code from last step to use "else if" instead of lots of if statements.
-
       // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
         if (userGuess === computerGuess) {
           wins++;
         }
         else if(userGuess !== computerGuess) {
             losses++;
+            guessesLeft=guessesLeft-1;
+            console.log(guessesLeft);
+            
+
         };
 
         //----------------------------------------------------------------------------
 // Set up an array of numbers.
-      var nums = [1, 8, 2, 3, 1, 1, 6, 6, 6];
+      //var nums = [1, 8, 2, 3, 1, 1, 6, 6, 6];
 
-      // Loop through the array we just set up.
-      for (var i = 0; i < nums.length; i++) {
-        // If the current number is greater than 2..
-        if (nums[i] > 2) {
-          // Log "number is greater than 2".
-          console.log("number is greater than 2");
-        }
-        // Otherwise
-        else {
-          // Log "number is not greater than 2"
-          console.log("number is not greater than 2");
-        }
-      }
+    //   // Loop through the array we just set up.
+    //   for (var i = 0; i < computerChoices.length; i++) {
+    //     // If the current number is greater than 2..
+    //     if (computerChoices[i] > 2) {
+    //       // Log "number is greater than 2".
+    //       console.log("number is greater than 2");
+    //     }
+    //     // Otherwise
+    //     else {
+    //       // Log "number is not greater than 2"
+    //       console.log("number is not greater than 2");
+    //     }
+    //   }
 
 //  // For-Loop that will repeat three times.
 //  for (var i = 1; i < 4; i++) {
@@ -86,6 +98,9 @@
           "<p>The computer chose: " + computerGuess + "</p>" +
           "<p>wins: " + wins + "</p>" +
           "<p>losses: " + losses + "</p>";
+          "<p>Guesses Left: " + guessesLeft + "</p>";
+
+         // "<p>losses: " + nums + "</p>";
         //   "<p>losses: " + favTVshows + "</p>";
         //   "<p>losses: " + tvShow + "</p>";
 
