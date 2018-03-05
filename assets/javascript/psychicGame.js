466a887be96961024194c8a315f0591b75efb431
@Donskoy <diman@arocketmail.com>
2018-03-05 05:21:58 -0600

    almost final

--- assets/javascript/psychicGame.js
+++ assets/javascript/psychicGame.js
@@ -1,14 +1,21 @@
 
 // Creates an array that lists out all of the options (various letters).
-    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    
-    // Creating variables to hold the number of wins, losses, and ties. They start at 0.
-    var wins = 0;
-    var losses = 0;
-    var guessesLeft = 9;
-    var lettersGuessed = [];
-    var computerGuess = [];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// Creating variables to hold the number of wins, losses, and ties. They start at 0.
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var lettersGuessed = [];
+var computerGuess = [];
+
+
+//need computer to guess start initial letter
+    window.onload = function() {
+      var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+      computerGuess.push(compGuess);
+        console.log(computerGuess[0]);
+    }
 
     // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
@@ -17,30 +24,37 @@
         console.log(userGuess)
 
 
-      // Randomly chooses a choice from the options array. This is the Computer's guess.
-      var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-      computerGuess.push(compGuess);
-        console.log(computerGuess[0]);
-
       // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
         if ((userGuess === computerGuess[0]) && (guessesLeft >0)){
           wins++;
-
+          guessesLeft = 9;
+          lettersGuessed.length = 0;
+          computerGuess.length = 0;
+          var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+          computerGuess.push(compGuess);
+          console.log(computerGuess[0]);
         }
+
         else if((userGuess !== computerGuess[0]) && (guessesLeft >0)) {
             guessesLeft=guessesLeft-1;
-            console.log(guessesLeft);
-            
-            
+            console.log(guessesLeft);            
         }
+
         else {
             losses++;
-
+            guessesLeft = 9;
+            lettersGuessed.length = 0;
+            computerGuess.length = 0;
+	        var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	        computerGuess.push(compGuess);
+	        console.log(computerGuess[0]);
         }
 
+
+        
         // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
         var html =
-        "<p>The computer chose: " + computerGuess + "</p>" +
+       // "<p>The computer chose: " + computerGuess + "</p>" +
         "<p>You chose: " + userGuess + "</p>" +
           "<p>wins: " + wins + "</p>" +
           "<p>losses: " + losses + "</p>" +
