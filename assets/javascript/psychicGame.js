6325ee7000be2b825b86157950b3849954868542
@Donskoy <diman@arocketmail.com>
2018-03-05 06:10:14 -0600

    almost final with graphics

--- assets/javascript/psychicGame.js
+++ assets/javascript/psychicGame.js
@@ -9,12 +9,26 @@ var guessesLeft = 9;
 var lettersGuessed = [];
 var computerGuess = [];
 
+var userGuess = " "
+var ascii9 = "ASCII9 ART";
 
 //need computer to guess start initial letter
     window.onload = function() {
       var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
       computerGuess.push(compGuess);
         console.log(computerGuess[0]);
+
+        var html =
+         "<p>You chose: " + userGuess + "</p>" +
+         "<p>wins: " + wins + "</p>" +
+         "<p>losses: " + losses + "</p>" +
+         "<p>Guesses Left: " + guessesLeft + "</p>" +
+         "<p>Your guesses so far: " + lettersGuessed + "</p>";
+          document.querySelector("#game").innerHTML = html;
+
+         var html2 =
+          "<p>" + ascii9 + "</p>" ;
+          document.querySelector("#game2").innerHTML = html2;
     }
 
     // This function is run whenever the user presses a key.
@@ -50,18 +64,40 @@ var computerGuess = [];
 	        console.log(computerGuess[0]);
         }
 
+        if (guessesLeft === 9){
+            var ascii9 = "ASCII9 ART";
+        }
 
-        
         // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
         var html =
        // "<p>The computer chose: " + computerGuess + "</p>" +
         "<p>You chose: " + userGuess + "</p>" +
-          "<p>wins: " + wins + "</p>" +
-          "<p>losses: " + losses + "</p>" +
-          "<p>Guesses Left: " + guessesLeft + "</p>" +
-          "<p>Your guesses so far: " + lettersGuessed + "</p>";
+        "<p>wins: " + wins + "</p>" +
+        "<p>losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + guessesLeft + "</p>" +
+        "<p>Your guesses so far: " + lettersGuessed + "</p>";
 
         // Set the inner HTML contents of the #game div to our html string
         document.querySelector("#game").innerHTML = html;
+
+        
+
+
+        // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
+        var html2 =
+        "<p>" + ascii9 + "</p>" ;
+        // "<p>" + ascii8 + "</p>" +
+        // "<p>" + ascii7 + "</p>" +
+        // "<p>" + ascii6 + "</p>" +
+        // "<p>" + ascii5 + "</p>" +
+        // "<p>" + ascii4 + "</p>" +
+        // "<p>" + ascii3 + "</p>" +
+        // "<p>" + ascii2 + "</p>" +
+        // "<p>" + ascii1 + "</p>" +
+        // "<p>" + ascii0 + "</p>";
+
+        // Set the inner HTML contents of the #game div to our html string
+        document.querySelector("#game2").innerHTML = html2;
+
       }
     
